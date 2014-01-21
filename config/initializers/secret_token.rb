95228d6fa8c359e87132d9abd12f3190b748ed62
Richard Pannell <richard.pannell@gmail.com>
2014-01-21 13:26:54 -0400

    Adding empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WanderStream::Application.config.secret_key_base = '3b23001ebb4cc7251fd431acb63da2fbf0550b6c56bfc40aac2781ebf18744469e893171c20e471eb85a1010886390ae7d1028ac35ce1a83e33b2a23ccd664fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WanderStream::Application.config.session_store :cookie_store, key: '_wander_stream_session'
