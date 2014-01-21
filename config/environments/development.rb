499b1cb87e47260cbdd305b84a9cd623fd0f84d2
Richard Pannell <richard.pannell@gmail.com>
2014-01-21 17:59:19 -0400

    testing devise and postgres

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ WanderStream::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Send mail from local machine
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
