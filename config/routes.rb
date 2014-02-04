be194abf6ea07d9d5d3b9fc5f33f40a0df6c5e9a
RailsDev <railsdev@railsdev-VirtualBox.(none)>
2014-02-04 12:18:43 -0400

    first map commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WanderStream::Application.routes.draw do
+  get "map/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
