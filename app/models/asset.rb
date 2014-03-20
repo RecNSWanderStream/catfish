daef0463c3c2f75668f5aa20afaf17b6b096ea07
fusionamoeba <railsdev@railsdev-VirtualBox.(none)>
2014-03-19 21:24:43 -0300

    columns added to query results

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -25,7 +25,7 @@ class Asset < ActiveRecord::Base
     #in_string = tokens.map!{|t| "'" + t + "'"}.join(',')
   
 
-    results = self.find_by_sql([ "select distinct a.name,a.id,a.description,at.name, a.lat,a.lng, r.name,a.is_active,a.washrooms,a.parking,a.accessibility_access, a.accessibility_information,a.time_open,a.time_closed,a.public_transit,a.nearest_city_id " +  
+    results = self.find_by_sql([ "select distinct a.name,a.id,a.description,at.name, a.lat,a.lng,r.id, r.name,a.is_active,a.washrooms,a.parking,a.accessibility_access, a.accessibility_information,a.time_open,a.time_closed,a.public_transit,a.nearest_city_id,aa.difficulty,aa.user_rating,aa.difficulty_rating,aa.is_loop,aa.distance " +  
      ", sum((case when r.name in (?) then 1 else 0 end)+(case when at.name in (?) then 1 else 0 end) + (1/cast(ati.origin_string_size as float))) as match_rating " +    
      "from asset_term_indices ati " +     
      "join assets a " +     
@@ -37,7 +37,7 @@ class Asset < ActiveRecord::Base
      "left join regions r " +
      "on a.region_id = r.id " +   
      "where ati.term in (?) or at.name in (?) or r.name in (?) " +     
-     "group by a.id, at.name,r.name  order by match_rating ", tokens,tokens,tokens,tokens,tokens])
+     "group by a.id, at.name,r.id,r.name,aa.difficulty,aa.user_rating,aa.difficulty_rating,aa.is_loop,aa.distance order by match_rating ", tokens,tokens,tokens,tokens,tokens])
   end
 end
 
