a59092d3208d4b147dd94d0a9b571ccbd55922b7
Richard Pannell <richard.pannell@gmail.com>
2014-03-26 23:34:30 +0000

    adding function to make sure asset names are lowercase

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,18 +4,9 @@ class Asset < ActiveRecord::Base
   has_many :activity_categories, :through => :activity_types
   belongs_to :city , :foreign_key => "nearest_city_id", :primary_key => "id"
   belongs_to :region
-#<-- 
-#  #validation for adding long and lat
-#  validates :lat, format: { with: /\A-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,6}/, message: "Must be valid latitude value with 1-6 decimals of precision"}
-
-# validates :lng, format: { with: /\A(\+|-)?(\d\.\d{1,6}|[1-9]\d\.\d{1,6}|1[1-7]\d\.\d{1,6}|180\.0{1,6})\Z/, message: "Must be valid longitude value with 1-6 decimals of precision"}
-#-->
 
 
-  #method to querey the database, put code inside class, it works better
-  def self.search_by_activity_type(activity)
-    self.joins(:asset_activities).joins(:activity_types).joins(:activity_categories).where(["activity_types.name = ? OR assets.name LIKE ? OR activity_categories.name = ?",activity,activity,activity])
-  end
+  before_save :downcase_name
 
   def self.switch_off
   	self.update(is_active: false)
@@ -40,6 +31,10 @@ class Asset < ActiveRecord::Base
      "group by r.name, a.id order by match_rating ", in_string,in_string,in_string,in_string,in_string])
   
     return results
+  end
+
+  def downcase_name
+    name.downcase!
   end
 end
 
